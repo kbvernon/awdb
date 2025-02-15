ee564fd9da77ec46f3a5079903e3818d1bac6463
kbvernon <kenneth.b.vernon@gmail.com>
2025-02-14 17:38:32 -0700

    add cascades polygon for examples

--- R/data.R
+++ R/data.R
@@ -1,10 +1,19 @@
-#' Bear Lake Area
+#' Datasets
 #'
-#' An arbitrary bounding box drawn around Bear Lake along the border between
-#' Utah and Idaho. See
+#' Arbitrary bounding boxes drawn around potential areas of interest.
+#'
+#' Areas of interest include:
+#' - Bear Lake along the border between Utah and Idaho. See
 #' [location on Google Maps](https://maps.app.goo.gl/bDBFyFhNgYbfvwuw8).
+#' - Cascades in west central Oregon. See
+#' [location on Google Maps](https://maps.app.goo.gl/sBtTHaN8SeM6xJWv7).
 #'
-#' @format ## `bear_lake`
-#' A simple feature column or `sfc` consisting of a single POLYGON geometry.
+#' @name datasets
+#' @keywords datasets
+#'
 #' @source Coordinates digitized manually.
 "bear_lake"
+
+#' @rdname datasets
+#' @format NULL
+"cascades"
--- data-raw/prepare-date.R
+++ data-raw/prepare-date.R
@@ -20,16 +20,31 @@ bear_lake <- st_as_sfc(bear_lake)
 
 usethis::use_data(bear_lake, overwrite = TRUE)
 
-# element codes
-element_codes <- read_csv("data-raw/element-codes.csv") |>
-  select(code, name, unit) |>
-  arrange(code)
+# just an arbitrary box around the Cascades in Oregon
+cascades <- st_bbox(
+  c(
+    xmin = -122.99948199993241,
+    ymin = 43.486155156558915,
+    xmax = -121.49548455805616,
+    ymax = 44.37787452173634
+  ),
+  crs = 4326
+)
 
-usethis::use_data(element_codes, overwrite = TRUE)
+cascades <- st_as_sfc(cascades)
+
+usethis::use_data(cascades, overwrite = TRUE)
+
+# element codes
+# element_codes <- read_csv("data-raw/element-codes.csv") |>
+#   select(code, name, unit) |>
+#   arrange(code)
+
+# usethis::use_data(element_codes, overwrite = TRUE)
 
 # network codes
-network_codes <- read_csv("data-raw/network-codes.csv") |>
-  select(code, description) |>
-  arrange(code)
+# network_codes <- read_csv("data-raw/network-codes.csv") |>
+#   select(code, description) |>
+#   arrange(code)
 
-usethis::use_data(network_codes, overwrite = TRUE)
+# usethis::use_data(network_codes, overwrite = TRUE)
