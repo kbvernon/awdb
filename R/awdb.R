d2771ea808428d75171a1002def644fd379894c8
kbvernon <kenneth.b.vernon@gmail.com>
2025-02-16 10:26:11 -0700

    package R script

---
+++ R/awdb.R
@@ -0,0 +1,11 @@
+#' Query the USDA AWDB REST API
+#'
+#' The awdb package implements functions to interface with the AWDB REST API
+#' from R.
+#'
+#' @name awdb
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom rlang %||%
+NULL
